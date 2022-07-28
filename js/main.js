7124a342b3b236243efd9d2f13369f00035ad294
clarac94 <claracastrodesign@gmail.com>
2022-07-27 22:32:07 -0300

    Smoot scroll

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//Carousel
+
+$('.carousel').carousel({
+    interval: 3000,
+    pause: false
+    })
+
+//Invisible navbar
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("navbar").style.top = "0";
+  } else {
+    document.getElementsByClassName("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
